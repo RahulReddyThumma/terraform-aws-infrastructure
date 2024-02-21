3be6179ebfe92142c4a6fe3e9c73e6ac9a8c7072
Rahul Reddy Thumma <thummarenoncerahul@gmail.com>
2024-02-21 15:01:28 +0530

    added s3 bucket

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
 provider "aws" {
   region = var.aws_region
 }
+
+terraform {
+  backend "s3" {
+    bucket = "my-terraform-state-bucket-rahulreddythumma"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+    dynamodb_table = "my-terraform-state-lock"
+  }
+}
